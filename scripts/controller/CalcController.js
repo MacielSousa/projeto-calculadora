80c5d1e20defdf0a6377bf7a504771c95c9018c0
adm.palmas2 <adm.palmas2@ensino.ulbra.net>
2019-12-10 11:55:32 -0300

    Erro

--- scripts/controller/CalcController.js
+++ scripts/controller/CalcController.js
@@ -1,6 +1,8 @@
 class CalcController{
     
     constructor(){ 
+
+        this._operation = [];
         this._locale = 'pt-BR';
         this._displayCalcEl = document.querySelector("#display");
         this._dateEl = document.querySelector("#data");
@@ -18,14 +20,76 @@ class CalcController{
 
     }
 
+    addEventListenerAll(element ,events, fn){
+        events.split(' ').forEach(event => {
+            element.addEventListener(event, fn, false);
+        });
+    }
+
+    clearAll(){
+
+
+    }
+
+    clearEntry(){
+
+    }
+    setError(){
+        this.displayCalc = "Error";
+    }
+
+    execBtn(value){
+        switch(value){
+            case 'ac':
+                this.clearAll();
+            break;
+            case 'ce':
+                this.clearEntry();
+            break;
+            case 'soma':
+                
+            break;
+            case 'subtracao':
+                
+            break;
+            case 'divisao':
+                
+            break;
+            case 'multiplicacao':
+                
+            break;
+            case 'porcento':
+                
+            break;
+            case 'igual':
+                
+            break;
+            
+            default:
+                this.setError();
+            break;
+
+        }
+    }
+
     iniButtonsEvents(){
         let buttons = document.querySelectorAll("#buttons > g, #parts > g");
         
         buttons.forEach((btn, index)=>{
-            btn.addEventListener('click', e => {
-                console.log(btn.className.baseVal.replace("btn-",""));
-            })
+            this.addEventListenerAll(btn, 'click drag', e => {
+                let textBtn = btn.className.baseVal.replace("btn-","");
+
+                this.execBtn();
+
+            });
+
+            this.addEventListenerAll(btn, "mouseover mouseup mousedown", e => {
+                btn.style.cursor = "pointer";
+            });
+
         });
+
+
 
     }
 
@@ -60,8 +124,8 @@ class CalcController{
         return this._displayCalcEl.innerHTML;
     }
 
-    set displayCalc(_value){
-        return this._displayCalcEl.innerHTML = valor;
+    set displayCalc(value){
+        return this._displayCalcEl.innerHTML = value;
     }
 
     get currentDate(){
